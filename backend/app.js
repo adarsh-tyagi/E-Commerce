894c582ca84933ff964b35aca5a22194f8dcaf6f
Tyagi1999 <adarshtyagi044@gmail.com>
2021-11-14 20:39:57 +0530

    added: orders model, routes

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,12 @@ app.use(cookieParser())
 // Route imports
 const productRouter = require("./routes/productRoute")
 const userRouter = require("./routes/userRoute")
+const orderRouter = require("./routes/orderRoute")
 
 
 app.use("/api/v1", productRouter)
 app.use("/api/v1", userRouter);
+app.use("/api/v1", orderRouter)
 
 // middlewares
 app.use(errorMiddleware)
---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  updateOrderStatus,
+  getAllOrders,
+  deleteOrder,
+} = require("../controllers/orderController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+const router = express.Router();
+
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrderStatus)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+
+module.exports = router;
