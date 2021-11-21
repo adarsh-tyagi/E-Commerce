b42d054c875fb91a2ebf70702a51d9891b32af1a
Tyagi1999 <adarshtyagi044@gmail.com>
2021-11-21 21:13:54 +0530

    updated: product details page

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProductDetails } from "../../actions/productAction";
 import { useParams } from "react-router";
 import ReactStars from "react-rating-stars-component";
+import ReviewCard from "./ReviewCard";
+import Loader from "../layout/Loader/Loader";
 
 function ProductDetails() {
   const dispatch = useDispatch();
@@ -21,6 +23,7 @@ function ProductDetails() {
     value: product.ratings,
     isHalf: true,
   };
+  console.log(options);
 
   useEffect(() => {
     dispatch(getProductDetails(id));
@@ -28,53 +31,67 @@ function ProductDetails() {
 
   return (
     <React.Fragment>
-      <div className="ProductDetails">
-        <div>
-          <Carousel>
-            {product.images &&
-              product.images.map((item, i) => (
-                <img
-                  className="CarouselImage"
-                  key={item.url}
-                  src={item.url}
-                  alt={`${i} Slide`}
-                />
-              ))}
-          </Carousel>
-        </div>
-        
-        <div>
-          <div className="detailsBlock-1">
-            <h2>{product.name}</h2>
-            <p>Product # {product._id}</p>
-          </div>
-          <div className="detailsBlock-2">
-            <ReactStars {...options} />
-            <span>({product.numOfReviews} Reviews)</span>
-          </div>
-          <div className="detailsBlock-3">
-            <h1>{`Rs. ${product.price}`}</h1>
-            <div className="detailsBlock-3-1">
-              <div className="detailsBlock-3-1-1">
-                <button>-</button>
-                <input value="1" type="number" />
-                <button>+</button>
-              </div>{" "}
-              <button>Add to Cart</button>
+      {loading ? (
+        <Loader />
+      ) : (
+        <React.Fragment>
+          <div className="ProductDetails">
+            <Carousel>
+              {product.images &&
+                product.images.map((item, i) => (
+                  <img
+                    className="CarouselImage"
+                    key={item.url}
+                    src={item.url}
+                    alt={`${i} Slide`}
+                  />
+                ))}
+            </Carousel>
+
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <ReactStars {...options} />
+                <span>({product.numOfReviews} Reviews)</span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`Rs. ${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button>-</button>
+                    <input value="1" type="number" />
+                    <button>+</button>
+                  </div>{" "}
+                  <button>Add to Cart</button>
+                </div>
+                <p>
+                  Status:{" "}
+                  <b className={product.stock < 1 ? "redColor" : "greenColor"}>
+                    {product.stock < 1 ? "Out of Stock" : "In Stock"}
+                  </b>
+                </p>
+              </div>
+              <div className="detailsBlock-4">
+                Description: <p>{product.description}</p>
+              </div>
+              <button className="submitReview">Submit Review</button>
             </div>
-            <p>
-              Status:{" "}
-              <b className={product.stock < 1 ? "redColor" : "greenColor"}>
-                {product.stock < 1 ? "Out of Stock" : "In Stock"}
-              </b>
-            </p>
           </div>
-          <div className="detailsBlock-4">
-            Description: <p>{product.description}</p>
-          </div>
-          <button className="submitReview">Submit Review</button>
-        </div>
-      </div>
+
+          <h3 className="reviewsHeading">REVIEWS</h3>
+          {product.reviews && product.reviews[0] ? (
+            <div className="reviews">
+              {product.reviews &&
+                product.reviews.map((review) => <ReviewCard review={review} />)}
+            </div>
+          ) : (
+            <p className="noReviews">No Reviews Yet</p>
+          )}
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 }
